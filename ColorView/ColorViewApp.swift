7ce3cc75b151664ea1c8e619ed0a978cbca2a000
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-09-14 18:45:11 +0300

    Initial Commit

---
+++ ColorView/ColorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewApp.swift
+//  ColorView
+//
+//  Created by Кирилл on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
